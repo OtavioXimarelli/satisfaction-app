751ea118763f6c30da93b96d6a47be9e418bea60
Otávio Ximarelli <ximarellio@gmail.com>
2024-09-23 17:38:52 -0300

    UPDATE FOOTER

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,35 @@
 import React from 'react';
-import { FaGithub } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa'; // Importing social icons
 
-const Footer = () => (
-  <footer className="footer">
-    <p>Developed by Otávio Ximarelli</p>
-    <a href="https://github.com/OtavioXimarelli" target="_blank" rel="noopener noreferrer">
-      <FaGithub style={{ marginRight: '5px' }} />
-      My GitHub
-    </a>
-  </footer>
-);
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <h2>G Desempenho</h2>
+      <p>Inovação e excelência em cada projeto.</p>
+      <p>&copy; {new Date().getFullYear()} G Desempenho. Todos os direitos reservados.</p>
+      
+      <div className="social-icons">
+        <Link to="#" title="Facebook"><FaFacebook /></Link>
+        <Link to="#" title="Twitter"><FaTwitter /></Link>
+        <Link to="#" title="Instagram"><FaInstagram /></Link>
+        <Link to="#" title="LinkedIn"><FaLinkedin /></Link>
+        <a href="https://github.com/OtavioXimarelli" target="_blank" rel="noopener noreferrer" title="My GitHub">
+          <FaGithub style={{ marginRight: '5px' }} />
+        </a>
+      </div>
+
+      <div className="credits">
+        <p>
+          Developed by 
+          <a href="https://github.com/OtavioXimarelli" target="_blank" rel="noopener noreferrer" style={{ marginLeft: '5px' }}>
+            <FaGithub style={{ marginRight: '5px' }} />
+            Otavio Ximarelli
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
